9f979409cfbd4bd75d707276c08be85022e4f12f
Winston Zhao <winston.zhao@winstonzhao.me>
2021-05-29 13:59:12 -0400

    updating image detection

--- index.js
+++ index.js
@@ -3,14 +3,11 @@ import LocomotiveScroll from 'locomotive-scroll';
 const scroll = new LocomotiveScroll({
   el: document.querySelector('[data-scroll-container]'),
   smooth: true,
-  smartphone: {
-    smooth: true
-  },
   tablet: {
     smooth: true
   }
 });
 
-imagesLoaded(".locomotive-scroll", { background: true }, function () {
-  locoScroll.update();
+imagesLoaded("data-scroll-container", { background: true }, function () {
+  LocomotiveScroll.update();
 });
